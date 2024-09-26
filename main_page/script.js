acfe9c92584646b0250db6971d02ab3040908861
Hao Luu <luuquachgiahao99@gmail.com>
2024-09-26 20:33:50 +0700

    Added comments for the function that adds style to the button when clicked.\nCreated a function to repeatedly add button to the body of the main page (can be adjusted to add button based on database in the future).\nDeleted the buttons (options) from the main page.

--- main_page/script.js
+++ main_page/script.js
@@ -1,5 +1,23 @@
-const buttons = document.querySelectorAll(".option");
 
+
+// create a number of options
+// get the id of the body
+const container = document.getElementById("option-wrapper");
+for (let i = 0; i < 15; i++) {
+  // create a new button
+  const newElement = document.createElement("button");
+  // add content to the button
+  newElement.textContent = "Vegan";
+  // add class to the button
+  newElement.classList.add("option");
+  // add the element to the body
+  container.appendChild(newElement);
+}
+
+
+// create different styling for the button when clicked
+// make all the buttons "hear" the click event to toggle options
+const buttons = document.querySelectorAll(".option");
 buttons.forEach((button) => {
   button.addEventListener("click", function () {
     this.classList.toggle("active");
