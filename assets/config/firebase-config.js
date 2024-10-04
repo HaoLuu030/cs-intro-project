64b2d31318b4b4c03939687ebebb54823c0531ce
Hao Luu <luuquachgiahao99@gmail.com>
2024-10-04 08:14:49 +0700

    Created a separate file to store firebase config for resusability in the future

---
+++ assets/config/firebase-config.js
@@ -0,0 +1,15 @@
+// firebase config.js
+
+// firebase config object
+const firebaseConfig = {
+  apiKey: "AIzaSyC30Yz7Zct6gpTgFHCbVPDYVI2cTglkPmI",
+  authDomain: "food-4206b.firebaseapp.com",
+  projectId: "food-4206b",
+  storageBucket: "food-4206b.appspot.com",
+  messagingSenderId: "1056463919710",
+  appId: "1:1056463919710:web:d453c55df200e115d972cf",
+  measurementId: "G-PDP3EZH9T8",
+};
+
+
+export default firebaseConfig;
--- assets/script/add-store.js
+++ assets/script/add-store.js
@@ -10,15 +10,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
 // Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyC30Yz7Zct6gpTgFHCbVPDYVI2cTglkPmI",
-  authDomain: "food-4206b.firebaseapp.com",
-  projectId: "food-4206b",
-  storageBucket: "food-4206b.appspot.com",
-  messagingSenderId: "1056463919710",
-  appId: "1:1056463919710:web:d453c55df200e115d972cf",
-  measurementId: "G-PDP3EZH9T8",
-};
+import firebaseConfig from "../config/firebase-config.js";
 
 // Initialize Firebase and Firestore
 const app = initializeApp(firebaseConfig);
